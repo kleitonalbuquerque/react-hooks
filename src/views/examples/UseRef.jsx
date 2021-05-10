31305e0da009e54b92d74fa4e6de4da35f150733
kleitonalbuquerque <kleiton2102@gmail.com>
2021-05-10 13:19:22 -0300

    Utilizando useRef

--- src/views/examples/UseRef.jsx
+++ src/views/examples/UseRef.jsx
@@ -1,15 +1,76 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, { useEffect, useRef, useState } from "react";
+import PageTitle from "../../components/layout/PageTitle";
+import SectionTitle from "../../components/layout/SectionTitle";
+
+const merge = function (s1, s2) {
+  return [...s1]
+    .map(function (e, i) {
+      return `${e}${s2[i] || ""}`;
+    })
+    .join("");
+};
 
 const UseRef = (props) => {
-    return (
-        <div className="UseRef">
-            <PageTitle
-                title="Hook UseRef"
-                subtitle="Retorna um objeto mutável com a propriedade .current!"
-            />
-        </div>
-    )
-}
+  const [value1, setValue1] = useState("");
+  const [value2, setValue2] = useState("");
 
-export default UseRef
+  const count = useRef(0);
+  const myInput1 = useRef(null);
+  const myInput2 = useRef(null);
+
+  console.log(myInput1.current);
+
+  useEffect(
+    function () {
+      count.current = count.current + 1; // Não causa uma nova renderização no componente
+      myInput2.current.focus(); // ao digitar vai para o input 2
+    },
+    [value1] // Só muda o valor de current qd value for alterado
+  );
+
+  useEffect(
+    function () {
+      count.current++; // Não causa uma nova renderização no componente
+      myInput1.current.focus(); // ao digitar vai para o input 1
+    },
+    [value2] // Só muda o valor de current qd value for alterado
+  );
+
+  return (
+    <div className="UseRef">
+      <PageTitle
+        title="Hook UseRef"
+        subtitle="Retorna um objeto mutável com a propriedade .current!"
+      />
+      <SectionTitle title="Exercício #01" />
+      <div className="center">
+        <div>
+          <span className="text">Valor: </span>
+          {/* <span className="text">{value1} [</span> */}
+          <span className="text">{merge(value1, value2)} [</span>
+          <span className="text red">{count.current}</span>
+          <span className="text">]</span>
+        </div>
+        <input
+          type="text"
+          className="input"
+          ref={myInput1}
+          value={value1}
+          onChange={(e) => setValue1(e.target.value)}
+        />
+      </div>
+      <SectionTitle title="Exercício #02" />
+      <div className="center">
+        <input
+          type="text"
+          className="input"
+          ref={myInput2}
+          value={value2}
+          onChange={(e) => setValue2(e.target.value)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default UseRef;
